d3c2972f88330fa6dfe8bd240b558e518731c659
Marcelo Murakami <marcelo.ymurakami@gmail.com>
2020-02-01 17:32:43 -0300

    upload ft_strlcat.c

--- libft/ft_strlcat.c
+++ libft/ft_strlcat.c
@@ -1,33 +1,36 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mmurakam <mmurakam@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/01/26 19:25:01 by mmurakam          #+#    #+#             */
-/*   Updated: 2020/02/01 16:50:18 by mmurakam         ###   ########.fr       */
+/*   Created: 2020/02/01 15:45:46 by mmurakam          #+#    #+#             */
+/*   Updated: 2020/02/01 17:31:58 by mmurakam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h>
 
-size_t  ft_strlcpy(char *dest, const char *src, size_t n)
+size_t	ft_strlcat(char *dest, const char *src, size_t n)
 {
-    int               i;
-    int               len;
-    unsigned char     *psrc;
-    unsigned char     *pdest;
+	unsigned int 	i;
+	unsigned int	len;
+	unsigned char	*pdest;
+	unsigned char	*psrc;
 
-    i = 0;
-    len = 0;
-    psrc = (unsigned char *)src;
-    pdest = (unsigned char *)dest;
-    while (dest[i] != '\0')
-        i++;
-    while (src[len] != '\0')
+	i = 0;
+	len = 0;
+	pdest = (unsigned char *)dest;
+	psrc = (unsigned char *)src;
+	while (dest[i++] != '\0')
+		pdest++;
+	while (src[len] != '\0')
 		len++;
-    while (i-- > 0 && n-- > 0)
-        *pdest++ = *psrc++;
-    return ((unsigned int)len);
+	len = len + i;
+	while (i++ <= n - 1)
+		*pdest++ = *psrc++;
+	*pdest = '\0';
+	return((unsigned int)len - 1);
 }

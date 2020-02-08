b6da8b9731c464d2cc0a618deea8a5bd61c00474
Marcelo Murakami <marcelo.ymurakami@gmail.com>
2020-02-08 20:27:13 -0300

    Updated ft_memove

--- libft/ft_memmove.c
+++ libft/ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: mmurakam <mmurakam@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/25 19:03:34 by mmurakam          #+#    #+#             */
-/*   Updated: 2020/02/03 08:06:42 by mmurakam         ###   ########.fr       */
+/*   Updated: 2020/02/08 20:24:52 by mmurakam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,19 +15,16 @@
 void	*ft_memmove(void *dest, const void *src, size_t n)
 {
 	unsigned char *ptr1;
-	unsigned char *ptr2;
+	unsigned char temp[n+1];
 	size_t i;
 
+	ft_bzero(temp, n+1);
 	ptr1 = (unsigned char*)dest;
-	ptr2 = (unsigned char*)src;
+	ft_memcpy(temp, src, n);
 	if ((unsigned int)ft_strlen(dest) >= (unsigned int)n)
 		i = n;
 	else
 		i = ft_strlen(dest);
-	ft_bzero(dest, ft_strlen(dest));
-	if (ft_strlen((char *)ptr1) >= ft_strlen((char *)ptr2))
-		ft_memcpy(ptr1, ptr2, n);
-	else
-		ft_memcpy(ptr1, ptr2, i);
+	ft_memcpy(ptr1, temp, i);
 	return (dest);
 }

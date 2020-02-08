5f509d409e745858d740e7dbc40256a23f04ab83
Marcelo Murakami <marcelo.ymurakami@gmail.com>
2020-02-08 20:33:01 -0300

    Update ft_strchar

--- libft/ft_strchr.c
+++ libft/ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: mmurakam <mmurakam@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/01 18:23:07 by mmurakam          #+#    #+#             */
-/*   Updated: 2020/02/02 17:55:33 by mmurakam         ###   ########.fr       */
+/*   Updated: 2020/02/08 20:32:21 by mmurakam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,14 @@ char	*ft_strchr(const char *str, int c)
 	unsigned char *pstr;
 
 	pstr = (unsigned char *)str;
-	while (*pstr++ != '\0')
+	while (*pstr != '\0')
+	{
 		if (*pstr == c)
 			return ((char *)pstr);
+		pstr++;
+	}
+	if (*pstr == c)
+		return ((char *)pstr);
 	return (NULL);
 }
 

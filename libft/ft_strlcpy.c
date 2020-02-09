5b561f62965508a1df9c5709236eef503a95dc1f
Marcelo Murakami <marcelo.ymurakami@gmail.com>
2020-02-09 01:59:48 -0300

    Update ft_strlcpy

--- libft/ft_strlcpy.c
+++ libft/ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: mmurakam <mmurakam@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/26 19:25:01 by mmurakam          #+#    #+#             */
-/*   Updated: 2020/02/02 17:56:50 by mmurakam         ###   ########.fr       */
+/*   Updated: 2020/02/09 01:59:37 by mmurakam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,24 @@
 
 size_t	ft_strlcpy(char *dest, const char *src, size_t n)
 {
-    int               i;
-    int               len;
-    unsigned char     *psrc;
-    unsigned char     *pdest;
+	int	i;
+	int	len;
 
-    i = 0;
-    len = 0;
-    psrc = (unsigned char *)src;
-    pdest = (unsigned char *)dest;
-    while (dest[i] != '\0')
-        i++;
-    while (src[len] != '\0')
-		len++;
-    while (i-- > 0 && n-- > 0)
-        *pdest++ = *psrc++;
-    return ((unsigned int)len);
+	len = 0;
+	i = 0;
+	while (src[i] != '\0')
+		i++;
+	len = i;
+	if (n == 0)
+		return (len);
+	while (i > 0 && n - 1 > 0)
+	{
+		*dest = *src;
+		i--;
+		dest++;
+		src++;
+		n--;
+	}
+	*dest = '\0';
+	return ((unsigned int)len);
 }

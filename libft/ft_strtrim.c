58bc21936127b7e121effaa73af7d8a627c4d77a
Marcelo Murakami <marcelo.ymurakami@gmail.com>
2020-02-09 21:11:53 -0300

    Update Prototype ft_strtim

--- libft/ft_strtrim.c
+++ libft/ft_strtrim.c
@@ -6,52 +6,24 @@
 /*   By: mmurakam <mmurakam@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/09 04:33:12 by mmurakam          #+#    #+#             */
-/*   Updated: 2020/02/09 05:24:38 by mmurakam         ###   ########.fr       */
+/*   Updated: 2020/02/09 20:52:43 by mmurakam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_startrim(const char *s1, int c)
-{
-	if (*s1 == c)
-	{
-		s1++;
-		return (0);
-	}
-	else
-		return (1);
-}
-
-int	ft_endtrim(const char *s1, int c)
-{
-	if (*s1 == c)
-	{
-		s1--;
-		return (0);
-	}
-	else
-		return (1);
-}
-
-
 char	*ft_strtrim(char const *s1, char const *set)
 {
-	char			*substr;
-	size_t			size;
-	unsigned int	flag;
-	unsigned int	i;
+	size_t	n;
+	char	*substr;
 
-	flag = 0;
-	i = 0;
-	size = ft_strlen(s1);
-	substr = ft_calloc(ft_strlen(s1), sizeof(char));
-	while (flag == 0 && set[i] != '\0')
-	{
-		if (ft_startrim(s1, set[i]) == 1 && ft_endtrim(&s1[size], set[i] == 1));
-			flag = 1;
-		if (ft_endtrim == 0)
-			size--;
-	}
-	while (*s1 != s1[size])
+	n = ft_strlen(s1);
+	if ((unsigned char *)s1 == NULL)
+		return (NULL);
+	while (*s1 != '\0' && ft_strchr(set, *s1) != NULL)
+		s1++;
+	while (n > 0 && ft_strrchr(set, (unsigned int)s1[n]) != NULL)
+		n--;
+	substr = ft_substr(s1, 0, n + 1);
+	return (substr);
 }

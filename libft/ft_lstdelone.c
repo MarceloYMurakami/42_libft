f169dda23910aac2b1c3a8a5b31ee7cc0a912579
Marcelo Murakami <marcelo.ymurakami@gmail.com>
2020-02-21 17:48:02 -0300

    Fixed ft_lstdelone

--- libft/ft_lstdelone.c
+++ libft/ft_lstdelone.c
@@ -6,21 +6,16 @@
 /*   By: mmurakam <mmurakam@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/21 13:18:12 by mmurakam          #+#    #+#             */
-/*   Updated: 2020/02/21 17:17:37 by mmurakam         ###   ########.fr       */
+/*   Updated: 2020/02/21 17:43:23 by mmurakam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	del(t_list *lst)
-{
-	t_list *current;
-
-	current = lst;
-	current->content = NULL;
-}
-
 void	ft_lstdelone(t_list *lst, void (*del)(void *))
 {
-	del(lst);
+	if (lst == NULL)
+		return;
+	del(lst->content);
+	free(lst->content);
 }
